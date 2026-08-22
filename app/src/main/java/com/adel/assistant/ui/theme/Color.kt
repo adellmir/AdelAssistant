fb2785c8ee52fd0ae4e81ef979ab694a759b05dc
adellmir <adell.mir@gmail.com>
2026-08-22 22:44:58 +0330

    Create Color.kt

---
+++ app/src/main/java/com/adel/assistant/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.adel.assistant.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WorkPrimary = Color(0xFF2E6F5E)     // سبز تیره - بخش کاری
+val WorkPrimaryLight = Color(0xFFDCEEE8)
+
+val FinancePrimary = Color(0xFF1E5A8E)  // آبی - بخش مالی
+val FinancePrimaryLight = Color(0xFFDCE9F5)
+
+val ToolPrimary = Color(0xFF8A5A2E)     // قهوه‌ای - بخش ابزار (آینده)
+val ToolPrimaryLight = Color(0xFFF0E3D3)
+
+val Background = Color(0xFFF7F7F5)
+val Surface = Color(0xFFFFFFFF)
+val TextPrimary = Color(0xFF1C1C1C)
+val TextSecondary = Color(0xFF6B6B6B)
