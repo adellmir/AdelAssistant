f06e401212f980e971cadfaff5bea758769ce7dc
adellmir <adell.mir@gmail.com>
2026-08-22 22:31:08 +0330

    Create Routes.kt

---
+++ app/src/main/java/com/adel/assistant/navigation/Routes.kt
@@ -0,0 +1,15 @@
+package com.adel.assistant.navigation
+
+object Routes {
+    const val HOME = "home"
+
+    const val WORK = "work"
+    const val WORK_TUNNEL = "work/tunnel"
+    const val WORK_FREE = "work/free"
+    const val WORK_TASKS = "work/tasks"
+
+    const val FINANCE = "finance"
+    const val FINANCE_TUNNEL = "finance/tunnel"
+    const val FINANCE_FREE = "finance/free"
+    const val FINANCE_RECEIVABLES = "finance/receivables"
+}
