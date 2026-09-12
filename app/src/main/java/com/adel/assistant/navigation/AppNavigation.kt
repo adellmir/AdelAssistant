9fe142d608e415cc554dd701f098404fc3e8330e
Adel <adell.mir@gmail.com>
2026-09-12 19:06:01 +0330

    بسته کامل: فاکتور، تسک، نمایش نقشه، تخلیه دوربین

--- app/src/main/java/com/adel/assistant/navigation/AppNavigation.kt
+++ app/src/main/java/com/adel/assistant/navigation/AppNavigation.kt
@@ -9,6 +9,8 @@ import androidx.navigation.NavType
 import androidx.navigation.navArgument
 import com.adel.assistant.ui.HomeScreen
 import com.adel.assistant.ui.dxf.DxfConverterScreen
+import com.adel.assistant.ui.screens.DxfPreviewScreen
+import com.adel.assistant.ui.screens.TotalStationDumpScreen
 import com.adel.assistant.ui.screens.AreaScreen
 import com.adel.assistant.ui.screens.ChainageScreen
 import com.adel.assistant.ui.screens.DailyReportScreen
@@ -18,6 +20,8 @@ import com.adel.assistant.ui.screens.LocationScreen
 import com.adel.assistant.ui.screens.ProjectEventsScreen
 import com.adel.assistant.ui.screens.ProjectRegisterScreen
 import com.adel.assistant.ui.screens.ReceivablesScreen
+import com.adel.assistant.ui.screens.InvoiceScreen
+import com.adel.assistant.data.InvoiceLaunch
 import com.adel.assistant.ui.screens.SimpleRecordScreen
 import com.adel.assistant.ui.screens.TunnelFinanceSummaryScreen
 import com.adel.assistant.ui.screens.TunnelPointsScreen
@@ -25,8 +29,6 @@ import com.adel.assistant.ui.screens.TunnelReceiptsScreen
 import com.adel.assistant.ui.screens.TaskScreen
 import com.adel.assistant.ui.screens.TunnelStatusScreen
 import com.adel.assistant.ui.screens.TunnelWorklogScreen
-import com.adel.assistant.ui.screens.InvoiceScreen
-import com.adel.assistant.data.InvoiceLaunch
 import com.adel.assistant.ui.screens.ViaClaudeScreen
 import com.adel.assistant.ui.screens.FinanceStatusScreen
 import com.adel.assistant.ui.screens.DatabaseBackupScreen
@@ -156,13 +158,14 @@ fun AppNavigation() {
         composable(Routes.TOOL_DXF) {
             DxfConverterScreen(onBack = { navController.popBackStack() })
         }
-        
         composable(Routes.TOOL_DXF_PREVIEW) {
             DxfPreviewScreen(color = ToolPrimary, onBack = { navController.popBackStack() })
         }
         composable(Routes.TOOL_TOTAL_STATION) {
             TotalStationDumpScreen(color = ToolPrimary, onBack = { navController.popBackStack() })
         }
+
+
         composable(Routes.TOOL_GSI) {
             GsiConverterScreen(color = ToolPrimary, onBack = { navController.popBackStack() })
         }
